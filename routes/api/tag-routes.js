6fd31c837f828e034b804972406329db38f35695
Asher <asher.bergtraun@gmail.com>
2021-08-31 00:40:11 -0700

    tag routes: find one tag  route completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,6 +20,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        through: ProductTag
+      }
+    ]
+  })
+  .then((tags) => res.status(200).json(tags))
 });
 
 router.post('/', (req, res) => {
