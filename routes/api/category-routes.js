e5b8ca3af1b51d57b0b4a6d3d7137aaa5f441716
Asher <asher.bergtraun@gmail.com>
2021-08-31 00:28:03 -0700

    category routes: find one from id route completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,6 +15,13 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  .then((categories) => res.json(categories))
 });
 
 router.post('/', (req, res) => {
